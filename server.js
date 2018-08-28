8444a735ab37af3409373fbd7be4665470ddf536
pavel sprogis <pavel.sprogis@gmail.com>
2018-08-28 12:22:14 +0300

    added post todos

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const todos = [{
-    id: 1,
-    description: 'meet mom for lunch',
-    completed: false,    
-}, {
-    id: 2,
-    description: 'go to market',
-    completed: false
-}, {
-    id: 3,
-    description: 'finish work',
-    completed: true
-}];
+const todos = [];
+let todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('TODO API root');
@@ -37,6 +29,14 @@ app.get('/todos/:id', (req, res) => {
     })
 
     res.status(404).send();
+});
+
+app.post('/todos', (req, res) => {
+    const body = req.body;
+
+    todos.push(Object.assign({id: todoNextId++}, body));
+    
+    res.json(body);
 });
 
 app.listen(PORT, () => {
