d1377dd669c567ebb8d351e1c13973cbc1e6db9c
Pavel Sprogis <pavel.sprogis@gmail.com>
2018-09-05 00:58:07 +0300

    hash and salt passwords

--- models/user.js
+++ models/user.js
@@ -1,25 +1,51 @@
-module.exports = (sequelize, DataTypes) => sequelize.define('user', {
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: true,
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('user', {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
         },
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [7, 100],
+        salt: {
+            type: DataTypes.STRING,
         },
-    },
-}, {
-    hooks: {
-        beforeValidate: (user) => {
-            if (typeof user.email === 'string') {
-                user.email = user.email.toLowerCase();
-            }
+        password_hash: {
+            type: DataTypes.STRING,
         },
-    },
-});
+        password: {
+            type: DataTypes.VIRTUAL,
+            allowNull: false,
+            validate: {
+                len: [7, 100],
+            },
+            set(value) {
+                const salt = bcrypt.genSaltSync(10);
+                const hashedPassword = bcrypt.hashSync(value, salt);
+
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword);
+            },
+        },
+    }, {
+        hooks: {
+            beforeValidate: (user) => {
+                if (typeof user.email === 'string') {
+                    user.email = user.email.toLowerCase();
+                }
+            },
+        },
+    });
+
+    User.prototype.toPublicJSON = function () {
+        const json = this.toJSON();
+
+        return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+    };
+    return User;
+};
